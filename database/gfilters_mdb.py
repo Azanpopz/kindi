aa592b4b9e24aa2e4f16a5d8050901f8de6c3c25
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-12-02 23:38:39 +0530

    Update gfilters_mdb.py

--- database/gfilters_mdb.py
+++ database/gfilters_mdb.py
@@ -1,100 +1,43 @@
 import pymongo
+
 from info import DATABASE_URI, DATABASE_NAME
-from pyrogram import enums
+
 import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
 myclient = pymongo.MongoClient(DATABASE_URI)
 mydb = myclient[DATABASE_NAME]
+mycol = mydb["USER"]
 
-
-
-async def add_gfilter(gfilters, text, reply_text, btn, file, alert):
-    mycol = mydb[str(gfilters)]
-    # mycol.create_index([('text', 'text')])
-
-    data = {
-        'text':str(text),
-        'reply':str(reply_text),
-        'btn':str(btn),
-        'file':str(file),
-        'alert':str(alert)
-    }
-
-    try:
-        mycol.update_one({'text': str(text)},  {"$set": data}, upsert=True)
-    except:
-        logger.exception('Some error occured!', exc_info=True)
-             
-     
-async def find_gfilter(gfilters, name):
-    mycol = mydb[str(gfilters)]
-    
-    query = mycol.find( {"text":name})
-    # query = mycol.find( { "$text": {"$search": name}})
-    try:
-        for file in query:
-            reply_text = file['reply']
-            btn = file['btn']
-            fileid = file['file']
+def insert(chat_id):
+            user_id = int(chat_id)
+            user_det = {"_id":user_id,"lg_code":None}
             try:
-                alert = file['alert']
+            	mycol.insert_one(user_det)
             except:
-                alert = None
-        return reply_text, btn, alert, fileid
-    except:
-        return None, None, None, None
+            	pass
 
+def set(chat_id,lg_code):
+	 mycol.update_one({"_id":chat_id},{"$set":{"lg_code":lg_code}})
 
-async def get_gfilters(gfilters):
-    mycol = mydb[str(gfilters)]
+	 	
+def unset(chat_id):
+	mycol.update_one({"_id":chat_id},{"$set":{"lg_code":None}})
 
-    texts = []
-    query = mycol.find()
-    try:
-        for file in query:
-            text = file['text']
-            texts.append(text)
-    except:
-        pass
-    return texts
+def find(chat_id):
+	id =  {"_id":chat_id}
+	x = mycol.find(id)
+	for i in x:
+             lgcd = i["lg_code"]
+             return lgcd 
 
+def getid():
+    values = []
+    for key  in mycol.find():
+         id = key["_id"]
+         values.append((id)) 
+    return values
 
-async def delete_gfilter(message, text, gfilters):
-    mycol = mydb[str(gfilters)]
-    
-    myquery = {'text':text }
-    query = mycol.count_documents(myquery)
-    if query == 1:
-        mycol.delete_one(myquery)
-        await message.reply_text(
-            f"'`{text}`'  deleted. I'll not respond to that gfilter anymore.",
-            quote=True,
-            parse_mode=enums.ParseMode.MARKDOWN
-        )
-    else:
-        await message.reply_text("Couldn't find that gfilter!", quote=True)
-
-async def count_gfilters(gfilters):
-    mycol = mydb[str(gfilters)]
-
-    count = mycol.count()
-    return False if count == 0 else count
-
-
-async def gfilter_stats():
-    collections = mydb.list_collection_names()
-
-    if "CONNECTION" in collections:
-        collections.remove("CONNECTION")
-
-    totalcount = 0
-    for collection in collections:
-        mycol = mydb[collection]
-        count = mycol.count()
-        totalcount += count
-
-    totalcollections = len(collections)
-
-    return totalcollections, totalcount
+def find_one(id):
+	return mycol.find_one({"_id":id})
