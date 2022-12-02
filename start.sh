f4777503cbfaaabf6ffc918c0d8336df632f55f0
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-12-02 23:43:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Azanpopz/kindi.git /RashmikaBot
+  git clone https://github.com/Azanpopz/kindi.git /kindi
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /kindi
